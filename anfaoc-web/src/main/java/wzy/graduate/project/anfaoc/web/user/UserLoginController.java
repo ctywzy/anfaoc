4201c99c422f74d6d7c486ac86dd7a37af0f12d1
王祖瑀 <wb-wzy620737@alibaba-inc.com>
2019-12-09 15:08:55 +0800

    create project

---
+++ anfaoc-web/src/main/java/wzy/graduate/project/anfaoc/web/user/UserLoginController.java
@@ -0,0 +1,14 @@
+package wzy.graduate.project.anfaoc.web.user;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "用户登陆管理",description = "用来管理用户登陆相关的信息")
+@RequestMapping()
+@Slf4j
+public class UserLoginController {
+
+}
