303219c7b235c8805c0a7242d75ef695c0babbbe
王祖瑀 <wb-wzy620737@alibaba-inc.com>
2020-01-20 17:19:57 +0800

    test:新闻更新接口单测

---
+++ anfaoc-service/src/test/java/news/NewsTest.java
@@ -0,0 +1,17 @@
+package news;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = NewsTest.class)
+public class NewsTest {
+
+    @Test
+    public void updateNewsTest(){
+
+
+    }
+}
